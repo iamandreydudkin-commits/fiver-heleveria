09ddf7892e4de96af8bf018829f65238ccac1c21
danish2016 <danish.danish.danish2016@gmail.com>
2023-11-29 19:13:10 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <div id="hero" class="h-screen relative bg-primary mt-[100px]">
+    <img
+      class="z-[2] absolute mx-auto -top-6 right-0 left-3"
+      src="/hero.png"
+      alt=""
+    />
+    <img
+      class="z-[1] absolute mx-auto right-0 left-0"
+      src="/hero_bg.png"
+      alt=""
+    />
+    <div
+      class="z-10 absolute mx-auto right-0 left-0 top-[650px] text-4xl flex justify-center text-white"
+    >
+      <button></button>
+      <button></button>
+    </div>
+    dsa
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+/* #hero {
+  background-image: url("/hero_bg.png");
+  background-size: fit;
+  background-repeat: no-repeat;
+  background-position: center;
+} */
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Instragram from "@/components/icons/Instagram.vue";
+import Youtube from "@/components/icons/Youtube.vue";
+import Tiktok from "@/components/icons/Tiktok.vue";
+</script>
+
+<template>
+  <div class="z-30 fixed top-0 w-full bg-primary text-white">
+    <div class="pt-5 max-w-7xl flex justify-between items-center mx-auto">
+      <div>
+        <img src="/logo.png" width="80" alt="" />
+      </div>
+      <div class="flex gap-x-[60px]">
+        <a href="#">ГОЛОВНА</a>
+        <a href="#">КОНТАКТИ</a>
+        <a href="#">ПРО МЕНЕ</a>
+      </div>
+      <div class="flex gap-x-4">
+        <div
+          class="w-8 h-8 bg-white flex justify-center items-center rounded-full"
+        >
+          <Instragram />
+        </div>
+        <div
+          class="w-8 h-8 bg-white flex justify-center items-center rounded-full"
+        >
+          <Youtube />
+        </div>
+        <div
+          class="w-8 h-8 bg-white flex justify-center items-center rounded-full"
+        >
+          <Tiktok />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+a {
+  @apply hover:bg-slate-800 hover:text-white px-5 py-3 rounded-md;
+}
+</style>
